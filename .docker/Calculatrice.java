424ccd06298f0ac2f734d34c484a545f64b018da
Aimen Zemzmi <aimen.zemzmi@omendo.com>
2020-07-16 09:01:41 +0200

    calculatrice

---
+++ .docker/Calculatrice.java
@@ -0,0 +1,31 @@
+public class Calculatrice{
+
+
+    public int addition(int a, int b)
+    {
+        int res;
+        res = a + b;
+        return res;
+    }
+
+    public int division(int a, int b)
+    {
+        int res;
+        res = a / b;
+        return res;
+    }
+
+    public int multiplication(int a, int b)
+    {
+        int res;
+        res = a * b;
+        return res;
+    }
+
+    public int soustraction(int a, int b)
+    {
+        int res;
+        res = a - b;
+        return res;
+    }
+}
